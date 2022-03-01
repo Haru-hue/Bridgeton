d422658470f93d6a4bac08ebb91d48f41a734d02
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-03-01 14:57:58 +0100

    Respoinsiveness

--- components/footer.js
+++ components/footer.js
@@ -6,12 +6,12 @@ class Footer extends HTMLElement {
         this.innerHTML = `<footer class = "container-fluid pt-5">
         <div class = "container">
             <div class = "row pb-5">
-                <div class = "col-lg-6">
+                <div class = "col-lg-6 col-md-6 col-sm-4 col-3">
                     <div class = "footer-logo">Bridgerton</div>
                 </div>
-                <div class = "col-lg-6">
+                <div class = "col-lg-6 col-md-6 col-sm-8 col-9">
                     <div class = "row ps-5">
-                        <div class = "col-lg-3">
+                        <div class = "col-lg-6 col-md-6 col-sm-5 col-4">
                             <ul class = "footer-nav">
                                 <li>Delivery</li>
                                 <li>Returns</li>
@@ -19,7 +19,7 @@ class Footer extends HTMLElement {
                                 <li>Get Help</li>
                             </ul>
                         </div>
-                        <div class = "col-lg-4">
+                        <div class = "col-lg-6 col-md-6 col-sm-7 col-8">
                             <ul class = "footer-nav">
                                 <li>About Us</li>
                                 <li>Events</li>
--- components/header.js
+++ components/header.js
@@ -8,8 +8,8 @@ class Header extends HTMLElement {
       this.innerHTML = `
       
         <header class = "container-fluid">
-    <nav class = "navbar navbar-expand-lg d-flex">
-        <ul class="navbar-nav d-none d-md-flex ps-5">
+    <nav class = "navbar navbar-expand d-flex">
+        <ul class="navbar-nav ps-lg-5">
             <li class = "nav-item">
                 <a href = "404.html" class="nav-link">Top Deals</a>
             </li>
@@ -21,13 +21,13 @@ class Header extends HTMLElement {
             </li>
         </ul>
         <a href = "index.html" class = "navbar-brand">Bridgerton</a>
-        <ul class="navbar-nav ms-auto pe-5">
+        <ul class="navbar-nav ms-auto pe-lg-5">
             <input type="text" placeholder="Search" id="bar">
             <li class = "nav-item">
                 <i class="fas fa-search pt-2" id="search-bar" onclick="foo()"></i>
             </li>
             <li class = "nav-item">
-                <a href = "login.html" class="nav-link"><div>Sign in</div></a>
+                <a href = "login.html" class="nav-link" id="sign-in"><div>Sign in</div></a>
             </li>
             <li class = "nav-item">
                 <i class="fas fa-shopping-bag pt-2"></i>
