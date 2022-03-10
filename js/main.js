99ef01a8deddc1ccefcaa1d7a5339aec943bb2f3
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-03-10 08:54:54 +0100

    Just some little changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // To append 1 to 30 to the select 
 $(document).ready(function(){
     var select = $(".quan");
@@ -5,6 +6,9 @@ $(document).ready(function(){
         select.append($('<option></option>').val(i).html(i))
     }
 
+   
+
+    //Changing background images
     const thumbnails = document.querySelectorAll(".view > div");
 
     const setBackground = function(event) {
@@ -18,16 +22,36 @@ $(document).ready(function(){
       thumbnail.addEventListener("click", setBackground);
     }
 
+  // Changing the option to buttons
     $(".sizes option").unwrap().each(function() {
       var btn = $('<button class = "btn btn-option">' + $(this).text() + '</button>')
       $(this).replaceWith(btn)
   })
-
+          
   $(document).on('click', '.btn-option', function() {
+    var text = $(this).text()
+    alert(text)
     $(".btn-option").removeClass("on");
     $(this).addClass("on");
-})
+
+    var innerWidth = $("body").innerWidth()
+    if(innerWidth < 768) {
+
+      
+      if(text === "S") {
+        $("#desc span").text("Small")
+      } else if (text === "M") {
+        $("#desc span").text("Medium")
+      } else if (text === "L") {
+        $("#desc span").text("Large")
+      } else if (text === "XL") {
+        $("#desc span").text("X-Large")
+      } else if (text === "XXL") {
+        $("#desc span").text("XX-Large")
+      }
+    } else {
+      $("desc span").remove()
+    }
+    
+  })
 });
-
-
-
