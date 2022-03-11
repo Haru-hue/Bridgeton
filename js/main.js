1e56ff1f37e48c2da4fcb755433093770490ad0f
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-03-11 17:03:07 +0100

    Moved from JQuery to VanillaJS

--- js/main.js
+++ js/main.js
@@ -1,57 +1,70 @@
+//Change image on click
+const thumbnails = document.querySelectorAll(".view > div");
+  const setBackground = function(event) {
+  const mainImage = document.getElementById("slider");
+  if (event.target && event.target.dataset.image) {
+    mainImage.style.backgroundImage = `url(${event.target.dataset.image})`;
+  }
+}
+  
+for(const thumbnail of thumbnails) {
+  thumbnail.addEventListener("click", setBackground);
+}
 
-// To append 1 to 30 to the select 
-$(document).ready(function(){
-    var select = $(".quan");
-    for (i=1;i<=30;i++){
-        select.append($('<option></option>').val(i).html(i))
+//unwraps the select option
+function unwrap(elems) {
+  elems = 'length' in elems ? elems : [elems];
+  for (var i = 0; i < elems.length; i++) {
+      var elem        = elems[i];
+      var parent      = elem.parentNode;
+      var grandparent = parent.parentNode;
+
+      grandparent.insertBefore(elem, parent);
+
+      if (parent.children.length === 0) 
+          grandparent.removeChild(parent);
+  }
+}
+
+var option = document.querySelectorAll(".sizes option")   
+unwrap(option);
+
+//Making the unwrapped class buttons
+var button = document.querySelectorAll("#buttonsize option")
+button.forEach(item => { 
+  let btn = document.createElement("button")
+  btn.innerHTML = item.textContent
+  item.parentNode.replaceChild(btn, item);
+})
+
+//Setting the attributes to the class
+var newb = document.querySelectorAll("#buttonsize button")
+for(var i = 0;i < newb.length; ++i) {
+  newb[i].setAttribute("class", "btn btn-option")
+}
+
+newb.forEach( (btClickEv,_,buttons) =>  {
+  btClickEv.onclick = () =>
+    buttons.forEach(bt=>bt.classList.toggle('on',bt===btClickEv))
+})
+
+function append () {
+  if(window.innerWidth < 768) {
+    var arSizes = ["Small", "Medium", "Large", "X-Large", "Large", "XX-Large"]
+    for(var i = 0; i <= arSizes.length; i++) {
+      newb[i].textContent = arSizes[i]
     }
+  } 
+}
 
-   
+window.addEventListener('resize', append())
 
-    //Changing background images
-    const thumbnails = document.querySelectorAll(".view > div");
+var select = document.querySelector(".quan")
+var contents
 
-    const setBackground = function(event) {
-    const mainImage = document.getElementById("slider");
-        if (event.target && event.target.dataset.image) {
-          mainImage.style.backgroundImage = `url(${event.target.dataset.image})`;
-        }
-    }
+for(var i=1; i<=30;i++) {
+  contents+= "<option>" + i + "</option>"
+}
 
-    for(const thumbnail of thumbnails) {
-      thumbnail.addEventListener("click", setBackground);
-    }
+select.innerHTML = contents
 
-  // Changing the option to buttons
-    $(".sizes option").unwrap().each(function() {
-      var btn = $('<button class = "btn btn-option">' + $(this).text() + '</button>')
-      $(this).replaceWith(btn)
-  })
-          
-  $(document).on('click', '.btn-option', function() {
-    var text = $(this).text()
-    alert(text)
-    $(".btn-option").removeClass("on");
-    $(this).addClass("on");
-
-    var innerWidth = $("body").innerWidth()
-    if(innerWidth < 768) {
-
-      
-      if(text === "S") {
-        $("#desc span").text("Small")
-      } else if (text === "M") {
-        $("#desc span").text("Medium")
-      } else if (text === "L") {
-        $("#desc span").text("Large")
-      } else if (text === "XL") {
-        $("#desc span").text("X-Large")
-      } else if (text === "XXL") {
-        $("#desc span").text("XX-Large")
-      }
-    } else {
-      $("desc span").remove()
-    }
-    
-  })
-});
